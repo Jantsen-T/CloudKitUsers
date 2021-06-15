a3d1cd8946b0047ed16fd4c2ce72b95162663aa1
Jantsen Tanner <jantsentanner@gmail.com>
2021-06-15 11:35:04 -0600

    added a helpers folder

--- CloudKitUsersTest/CloudKitUsersTest/Controllers/ModelControllers/UserController.swift
+++ CloudKitUsersTest/CloudKitUsersTest/Controllers/ModelControllers/UserController.swift
@@ -18,10 +18,10 @@ class UserController {
     //MARK: - CRUD Functions
     
 //    func createUserWith(userName: String, profilePic: UIImage, bio: String, experienceLevel: String, location: String, instrument: String, styleTastes: String, completion: @escaping (Result<User?, UserError>) -> Void) {
-//        
-//        
+//
+//
 //        let newUser = User(username: userName, profilePic: profilePic, experienceLevel: experienceLevel, bio: bio, location: location, instrument: instrument, styleTastes: styleTastes)
-//        
+//
 //        let userRecord = CKRecord.init(user:)
 //        privateDB.save(userRecord) { record, error in
 //            if let error = error {
@@ -30,10 +30,10 @@ class UserController {
 //            guard let record = record,
 //                  let savedUser = User(ckRecord: record) else { return }
 //            completion(.failure(.couldNotUnwrap))
-//            
+//
 //        }
 //    }
-//    
-//    
+//
+//
 }// End of class
 
