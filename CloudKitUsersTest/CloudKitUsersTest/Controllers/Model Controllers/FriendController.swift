8cea18b6dfe51920084cf68c7dba5b3a4861b47e
Gavin Craft <borp@Gavins-MacBook-Pro.local>
2021-06-15 11:21:57 -0400

    gavin did the making user have an array and made an struct for fwen request

---
+++ CloudKitUsersTest/CloudKitUsersTest/Controllers/Model Controllers/FriendController.swift
@@ -0,0 +1,24 @@
+//
+//  FriendController.swift
+//  CloudKitUsersTest
+//
+//  Created by Gavin Craft on 6/15/21.
+//
+
+import Foundation
+import CloudKit
+class FriendController{
+    static let shared = FriendController()
+    var currentUser: User?
+    let privateDatabase = CKContainer.default().privateCloudDatabase
+    let publicDatabase = CKContainer.default().publicCloudDatabase
+    
+    //MARK: friend functions
+    func sendFriendRequest(username: String){
+        if username == ""{//if username exists, this is just a placeholder
+            guard let currentUser = currentUser else { return}
+            let friendRequest = FriendRequest(senderUsername: currentUser.username, receiverUsername: username)
+        }
+    }
+    func getUser(username: String, completion: @escaping(User)->Void)
+}
